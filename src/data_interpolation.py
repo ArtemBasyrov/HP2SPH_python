fixed normalization

src/data_interpolation.py
@@ -53,12 +53,12 @@ def transform_healpix_to_grid(healpix_map: jnp.array) -> (jnp.array, jnp.array):
     
     # Define function for vectorized FFT processing
     def process_equatorial_ring(ring_data):
-        fft_coeffs = jnp.fft.fft(ring_data, n=4 * nside)
+        fft_coeffs = jnp.fft.fft(ring_data, n=4 * nside, norm='ortho')
         return fft_coeffs
     
     def process_polar_ring(ring_data):
         num_pts = len(ring_data)
-        coeffs = jnp.fft.fft(ring_data, n=num_pts)
+        coeffs = jnp.fft.fft(ring_data, n=num_pts, norm='ortho') 
         k_vals = np.fft.fftfreq(num_pts) * num_pts
         phase_shift = jnp.exp(-1j * jnp.pi * k_vals / (2 * nside))
         corrected_coeffs = coeffs * phase_shift
@@ -72,7 +72,7 @@ def transform_healpix_to_grid(healpix_map: jnp.array) -> (jnp.array, jnp.array):
         return coeffs_padded
     
     def inverse_fft(fft_coeffs):
-        return jnp.fft.ifft(fft_coeffs, n=4 * nside).real
+        return jnp.fft.ifft(fft_coeffs, n=4 * nside, norm='ortho').real
     
 
     # Diving data into rings

src/nuFFT.py
@@ -20,12 +20,13 @@ def apply_nuFFT(mp: jnp.array) -> jnp.array:
     DFT_upsampled_lat = DFT_upsampled_lat * np.pi / 180 + np.pi/2
 
     # setting up the nufft calculations
-    mp = mp.astype(complex)
+    M = 4 * nside + 1  # Number of Fourier modes
+    N = len(latitudes)*2+2 # Number of non-uniform latitude samples
     plan = finufft.Plan(1, (4*nside+1,), n_trans=4*nside) # allows to reuse the same plan for all latitudes
     plan.setpts(DFT_upsampled_lat)
     fft_lat = plan.execute(mp.T) # batch accelerated calcualtion
 
-    return fft_lat.T
+    return fft_lat.T / np.sqrt(N) # normalise the output
 
 
 def inverse_nuFFT(fft_lat: jnp.array) -> jnp.array:
@@ -65,7 +66,7 @@ def inverse_nuFFT(fft_lat: jnp.array) -> jnp.array:
     plan.setpts(DFT_upsampled_lat)
     mp_reconstructed = plan.execute(fft_lat.T) # batch accelerated calcualtion
 
-    mp_reconstructed = mp_reconstructed.T / N # normalise the output
+    mp_reconstructed = mp_reconstructed.T / np.sqrt(N) # normalise the output
 
     return mp_reconstructed.real 
 