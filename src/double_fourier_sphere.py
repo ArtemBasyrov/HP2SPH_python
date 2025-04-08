optimization + fixed wrong normalization for polar rings FFTs

src/double_fourier_sphere.py
@@ -7,8 +7,7 @@ from .data_interpolation import create_latitude_array
 
 
 def DFS(mp: jnp.array, fft_coeff: jnp.array) -> (jnp.array, jnp.array):
-    south_part = jnp.flipud(mp)
-    south_part = jnp.flip(south_part, axis=1)
+    south_part = jnp.flip(mp)
     double_map = jnp.concatenate((mp, south_part), axis=0)
 
     double_map = interpolate_polar_rings(double_map)
@@ -19,9 +18,9 @@ def DFS(mp: jnp.array, fft_coeff: jnp.array) -> (jnp.array, jnp.array):
     # double the fft coefficients
     n_rings = fft_coeff.shape[0]
     double_fft = np.zeros((2*n_rings+2, fft_coeff.shape[1]), dtype=complex)
-    double_fft[0] = np.fft.fft(double_map[0], n=fft_coeff.shape[1])
+    double_fft[0] = np.fft.fft(double_map[0], n=fft_coeff.shape[1], norm='ortho')
     double_fft[1:n_rings+1] = fft_coeff[:]
-    double_fft[n_rings+1] = np.fft.fft(double_map[n_rings], n=fft_coeff.shape[1])
+    double_fft[n_rings+1] = np.fft.fft(double_map[n_rings], n=fft_coeff.shape[1], norm='ortho')
     double_fft[n_rings+2:] = south_part
 
     # apply FFT shift from numpy ordering to natural ordering

src/nuFFT.py
@@ -24,7 +24,8 @@ def apply_nuFFT(mp: jnp.array) -> jnp.array:
     N = len(latitudes)*2+2 # Number of non-uniform latitude samples
     plan = finufft.Plan(1, (4*nside+1,), n_trans=4*nside) # allows to reuse the same plan for all latitudes
     plan.setpts(DFT_upsampled_lat)
-    fft_lat = plan.execute(mp.T) # batch accelerated calcualtion
+    mp_T = mp.T.copy()
+    fft_lat = plan.execute(mp_T) # batch accelerated calcualtion
 
     return fft_lat.T / np.sqrt(N) # normalise the output
 